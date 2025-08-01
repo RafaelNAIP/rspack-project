b2b90722eb0ea2f54d34d7f391a9aa90be83b29f
RafaelNAIP <rafael.diasmm2000@gmail.com>
2025-08-01 07:59:04 -0300

    fix: unsed imports

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@rspack/cli";
+// import { defineConfig } from "@rspack/cli";
 import { rspack } from "@rspack/core";
 import { ReactRefreshRspackPlugin } from "@rspack/plugin-react-refresh";
 import { withZephyr } from 'zephyr-webpack-plugin';
